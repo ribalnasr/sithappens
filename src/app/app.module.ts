e9ef22cc6da17a4b7ec559586de4cf1d1cc82a91
Ribal Nasr <ribal.nasr@gmail.com>
2022-02-21 16:32:22 +0400

    Added @nguniversal.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { DataModule } from './modules/data/data.module';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     IonicModule.forRoot({ mode: 'ios' }),
     AppRoutingModule,
     FirebaseModule,
